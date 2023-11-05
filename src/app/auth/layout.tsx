daeb6e4ffeded8dba95b0d0f80acc7607d40525d
tony <sumonroytony@gmail.com>
2023-11-05 19:50:39 +0600

    feat: first commit

---
+++ src/app/auth/layout.tsx
@@ -0,0 +1,14 @@
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <section>
+      {/* Include shared UI here e.g. a header or sidebar */}
+      <nav>dffffffffffffffffffffffff</nav>
+
+      {children}
+    </section>
+  );
+}
---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,11 @@
+import Button from '@/components/buttons/Button';
+
+export default function LoginPage() {
+  return (
+    <div>
+      <Button>Hello </Button>
+
+      <div>dddddddddddddddddd</div>
+    </div>
+  );
+}
